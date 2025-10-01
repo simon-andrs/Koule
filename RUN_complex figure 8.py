c1360d7ab3572c420ae02f5849b25a4529be0a96
simon-andrs <sima.andrs@gmail.com>
2025-10-01 19:06:15 +0200

    Přidán popis

--- RUN_complex figure 8.py
+++ RUN_complex figure 8.py
@@ -1,3 +1,9 @@
+"""
+Hodnoty z: Simó, C.(2001). New families of solutions in N-body problems. In European Congress of Mathematics: Barcelona, July 10--14, 2000, Volume I (pp.101-115). Birkhäuser Basel.
+Počítáno v jednorozměrném komplexním prostoru - při reálném čase odpovídá dvourozměrnému reálnému.
+Časový krok má však malou komplexní složku, která způsobuje pomalé odchylování od dvourozměrného modelu.
+"""
+
 from main import Main
 
 m = Main(dim = 1, deltat = 0.001 + 0.000001j, g_const = 1)
@@ -6,3 +12,4 @@ m.obj((-0.995492,), (-0.695804/2 - 1.067860/2 * 1j,), r = 0.02, col = 'red')
 m.obj((0,), (0.695804 + 1.067860j,), r = 0.02, col = 'green')
 
 m.run()
+
