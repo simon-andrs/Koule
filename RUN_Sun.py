c0f7e9e5e5fd45ecedf0b4580c1eef30abc2d424
simon-andrs <sima.andrs@gmail.com>
2025-10-01 18:59:16 +0200

    Přidán popis

--- RUN_Sun.py
+++ RUN_Sun.py
@@ -1,3 +1,5 @@
+"""3D simulace Slunce a 3 družic se stejnou oběžnou dobou jako Země"""
+
 from main import Main
 m = Main(dim = 3, deltat = 2000)
 
@@ -8,3 +10,4 @@ m.obj((-1.327 * 10**11, 0, -2.298 * 10**10),(0, 32928, 0))
 m.obj((-1.496 * 10**11, 0, 0), (0, 29784, 0))
 
 m.run()
+
