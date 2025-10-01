dda036228286a16babd90ef66eb748c761f63de4
simon-andrs <sima.andrs@gmail.com>
2025-10-01 19:03:08 +0200

    Přidán zdroj počátečních hodnot

--- RUN_figure 8.py
+++ RUN_figure 8.py
@@ -1,3 +1,5 @@
+"""Hodnoty z: Simó, C.(2001). New families of solutions in N-body problems. In European Congress of Mathematics: Barcelona, July 10--14, 2000, Volume I (pp.101-115). Birkhäuser Basel."""
+
 from main import Main
 
 m = Main(dim = 2, deltat = 0.001, g_const = 1)
@@ -6,3 +8,4 @@ m.obj((-0.995492, 0), (-0.695804/2,-1.067860/2), r = 0.02, col = 'red')
 m.obj((0, 0), (0.695804,1.067860), r = 0.02, col = 'green')
 
 m.run()
+
